228b875fbfd26cd820103ef0593668d3392da0f8
zakaria.ben-allal <zakaria.ben-allal@epita.fr>
2020-08-17 18:37:38 +0200

    [WIP] display spotify new releases

--- challengegroover/routes.py
+++ challengegroover/routes.py
@@ -1,9 +1,10 @@
+import requests
 from flask import Blueprint, redirect, request, jsonify, render_template
 from .spotify_auth import SpotifyAuth
 
+
 # Landing page of the app.
 root = Blueprint("root", "root", url_prefix="/")
-
 
 @root.route("/")
 def index():
@@ -30,4 +31,49 @@ def callback():
 # API
 api = Blueprint("api", "api", url_prefix="/api")
 
-# Add your "artists" route here.
+@api.route('/artists')
+def get_user():
+    AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+    # POST
+    auth_response = requests.post(AUTH_URL, {
+        'grant_type': 'client_credentials',
+        'client_id': SpotifyAuth.CLIENT_ID,
+        'client_secret': SpotifyAuth.CLIENT_SECRET,
+    })
+
+    # convert the response to JSON
+    auth_response_data = auth_response.json()
+
+    # save the access token
+    access_token = auth_response_data['access_token']
+
+    url = 'https://api.spotify.com/v1/browse/new-releases?country=FR'
+    response = requests.request('GET', url, headers={
+        'Authorization': 'Bearer ' + access_token,
+    })
+
+    data = response.json()
+    return data
+    # code = request.values["code"]
+    # tokens = SpotifyAuth().getUserToken(code)
+# def fetch_artists():
+#     code = request.values["code"]
+#     tokens = SpotifyAuth().getUserToken(code)
+#     accessToken = jsonify(tokens)['access_token']
+#     return accessToken
+#     url = 'https://api.spotify.com/v1/browse/new-releases'
+#     return response.json()
+    # code = response.json().values["code"]
+    # tokens = SpotifyAuth().getUserToken(code)
+    # return '<h1>artists list loading...</h1>'
+
+
+# def api_all():
+#     conn = sqlite3.connect('books.db')
+#     conn.row_factory = dict_factory
+#     cur = conn.cursor()
+#     all_books = cur.execute('SELECT * FROM books;').fetchall()
+
+#     return jsonify(all_books)
+
